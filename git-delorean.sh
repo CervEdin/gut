f45f0b70ae90236720c211d9f7fd21d8f7f3cf0a
Erik Cervin Edin <erik@cervined.in>
2022-03-08 15:46:16 +0100

    Add git-delorean, a script that automatically creates fixup commits.
    The script *git-delorean* creates fixup commits to the commits
    that are touched by the individual staged chunks.
    
    Example:
     1.a
    ~1.b
    ~2.c
     1.d
    
    ~ denotes lines with staged changes, the digit a commit, the letters
    content on the line.
    
    git-delorean will create two fixup commits, one for commit "2", with
    contents "c" and another for "1" with contents "b".
    
    The commits are created in the order traversed by rev-list (which should
    be newest first?)

---
+++ git-delorean.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -euox pipefail
+
+GIT_ROOT=$(git rev-parse --show-toplevel)
+cd "$GIT_ROOT"
+STAGED=$(git diff --name-only --cached)
+git stash --keep-index
+
+IFS=$'\n'
+for FILE in $STAGED; do
+	# BUG: When there are only adds (@@ -XX,0 +YY,x) the first line will be XX
+	# It should probably be YY + x
+	LINES=$(
+		git diff --word-diff=porcelain -U0 --cached HEAD -- "$FILE" |\
+			awk -F '[, ]' '/^@@/ {
+					gsub(/^[^0-9]*/, "", $2)
+					r = 0
+					if (substr($3,1,1) != "+") {
+						r = gensub(/([0-9]*).*/, "\\1", "g", $3)
+					}
+					if (r != 0) {
+						print $2","$2 + r - 1
+					}
+					else {
+						print $2","$2
+					}}'
+	)
+	COMMITS=$(
+		xargs --verbose -I% git blame --incremental  -L % HEAD -- "$FILE" <<< "$LINES" |\
+			sed -n '/^[a-f,0-9]\{40\} /{s@ .*@@;p}' |\
+			awk '{ a[$1]++ } END { for (b in a) { print b }}'
+		)
+		# TODO: Verify the order of rev-list is the order we want (we may want to reverse?)
+		PARENT=$(git rev-list --no-walk $COMMITS | head -1)
+	git commit --fixup $PARENT -- "$FILE"
+done
+git stash pop
