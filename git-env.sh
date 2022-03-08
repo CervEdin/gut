6f9d4b4f088aaa1ec4aeece0fa63ffeb1a61e8b9
Erik Cervin-Edin <erik@cervined.in>
2022-03-08 16:06:35 +0100

    Add git enviroment establishing script.
    This script creates shortcuts for a few git commands.

---
+++ git-env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$GIT_ENV" == true ] ; then
+	echo "Exiting git enviroment"
+	unset -f aa
+	unset -f ss
+	unset -f sk
+	unset -f dff
+	unset -f sdf
+	unset -f cm
+	export GIT_ENV=false
+else
+	echo "Setting git enviroment"
+	git config commit.verbose true
+	aa () { git add --all ; }
+	ss () { git status ; }
+	sk () { git rebase --skip; }
+	dff () { git diff ; }
+	sdf () { git diff --staged ; }
+	cm () { git commit -m $1 ; }
+	export GIT_ENV=true
+fi
