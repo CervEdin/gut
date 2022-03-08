fd77bffb7a5cbff1bcc4efe6949853eed2e50d4e
Erik Cervin-Edin <erik@cervined.in>
2022-03-08 15:12:10 +0100

    Allow interactive picking of target in mebase
    Running
      git mebase -i
    Will produce a list
      1. a
      2. b
      3. c
      ...
    allowing the user to press 1,2,3.. to select a target parent.
    
    The default is to select the first parent, 1, according to git-parents.

--- git-mebase.sh
+++ git-mebase.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 set -euo pipefail
 
-TARGET=$(git parents | head -1)
-echo "Rebasing onto: $TARGET"
+INTERACTIVE=false
+
+while getopts ":i" opt; do
+	case $opt in
+		i)
+			INTERACTIVE=true
+			;;
+		\?)
+			printf "Invalid option: -$OPTARG\n"
+			exit 1
+			;;
+	esac
+done
+
+PARENTS=$(git parents)
+
+if [ "$INTERACTIVE" == true ] ; then
+	nl <<< "$PARENTS"
+	# TODO: Verify this works 10+
+	read -p "Pick parent: " N_PARENT
+else
+	N_PARENT=1
+fi
+
+PARENT=$(sed "${N_PARENT}q;d" <<< "$PARENTS")
 
 git arch &&\
-  git -c sequence.editor='git rebase-retag.sh' rebase -i -r --autosquash "$TARGET"
+  git -c sequence.editor='git rebase-retag.sh' rebase -i -r --autosquash "$PARENT"
