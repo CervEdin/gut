58c43fe7d2df09c8e0d1f9071fd5e0bdd628801c
Erik Cervin-Edin <erik@cervined.in>
2022-03-09 17:31:25 +0100

    Fix missing rebase-indent script
    - Add the missing rebase-indent sed script
    - Reference the file in git-rebase-retag
    NB:	The path is hardcoded to ~/bin/
    TODO: Embedd it in the .sh file?
    - Add .sed to makefile:install

--- git-rebase-retag.sh
+++ git-rebase-retag.sh
@@ -29,7 +29,7 @@ for TAG in $TAGS; do
 	SED_CMDs="$SED_CMDs;$SED_CMD"
 done
 
-sed -e "`cat rebase-indent.sed`" \
+sed -e "`cat $HOME/bin/git-rebase-indent`" \
 -e "$SED_CMDs" -i "$TODO"
 
 vim "$1"
