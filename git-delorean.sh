a23353252b4758871c43e33605336e5629c7ea4d
Erik Cervin-Edin <erik@cervined.in>
2022-05-18 09:14:32 +0200

    style:	lowercase local bash variables per convention
    > By convention, environment variables (`PAGER`, `EDITOR`, ...) and internal
    > shell variables (`SHELL`, `BASH_VERSION`, ...) are capitalized. All other
    > variable names should be lower case.
    >
    > Remember that variable names are case-sensitive; this convention avoids
    > accidentally overriding environmental and internal variables.
    >
    > Keeping to this convention, you can rest assured that you don't need to
    > know every environment variable used by UNIX tools or shells in order to
    > avoid overwriting them. If it's your variable, lowercase it. If you
    > export it, uppercase it.

--- git-delorean.sh
+++ git-delorean.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 set -euox pipefail
 
-GIT_ROOT=$(git rev-parse --show-toplevel)
-cd "$GIT_ROOT"
-STAGED=$(git diff --name-only --cached)
+git_root=$(git rev-parse --show-toplevel)
+cd "$git_root"
+staged=$(git diff --name-only --cached)
 git stash --keep-index
 
 IFS=$'\n'
-for FILE in $STAGED; do
+for file in $staged; do
 	# BUG: When there are only adds (@@ -XX,0 +YY,x) the first line will be XX
 	# It should probably be YY + x
-	LINES=$(
-		git diff --word-diff=porcelain -U0 --cached HEAD -- "$FILE" |\
+	lines=$(
+		git diff --word-diff=porcelain -U0 --cached HEAD -- "$file" |\
 			awk -F '[, ]' '/^@@/ {
 					gsub(/^[^0-9]*/, "", $2)
 					r = 0
@@ -25,13 +25,13 @@ for FILE in $STAGED; do
 						print $2","$2
 					}}'
 	)
-	COMMITS=$(
-		xargs --verbose -I% git blame --incremental  -L % HEAD -- "$FILE" <<< "$LINES" |\
+	commits=$(
+		xargs --verbose -I% git blame --incremental  -L % HEAD -- "$file" <<< "$lines" |\
 			sed -n '/^[a-f,0-9]\{40\} /{s@ .*@@;p}' |\
 			awk '{ a[$1]++ } END { for (b in a) { print b }}'
-		)
-		FIRST_PARENT=$(git rev-list --topo-order HEAD | grep "$COMMITS" | head -1)
-		git commit --fixup $FIRST_PARENT -- "$FILE"
+	)
+	first_parent=$(git rev-list --topo-order HEAD | grep "$commits" | head -1)
+	git commit --fixup $first_parent -- "$file"
 done
 
 git stash pop
