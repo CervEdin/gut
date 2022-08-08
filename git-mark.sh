c786a058757f70bcfc6442a76c02447df7835c08
Erik Cervin-Edin <erik@cervined.in>
2022-08-08 11:18:25 +0200

    Add git-mark.sh	which adds a "mark" to branches
    Example
    =======
    
    On branch main
    ```git mark under-review```
    will change the (local) branch name to `under-review/main`.
    
    Typing
    ```git mark under-review```
    again should not add `under-review` again.

---
+++ git-mark.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -euo
+
+if [ -z "$1" ]; then
+	printf "Fatal: argument needed" >&2 &&
+	exit 1
+fi
+
+mark="$1"
+
+git branch --show-current |\
+	sed 's@^'"$mark"'/@@' |\
+	xargs -I% git branch -m "$mark"'/'%
