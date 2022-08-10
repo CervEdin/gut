1353da67200dad154f4db5348274f73c1f3ec8e6
Erik Cervin-Edin <erik@cervined.in>
2022-08-10 17:43:10 +0200

    mark:	Change tag char from # to @
    `#` doesn't play nice with bash

--- git-mark.sh
+++ git-mark.sh
@@ -24,13 +24,13 @@ fi
 tag_char='@'
 
 for branch in $branches; do
-	# branch starts with #mark
+	# branch starts with $tag_char
 	if [[ $branch == $tag_char$mark/* ]]; then
 		[[ $add == true ]] &&
 			continue # already marked
 		git branch -m $branch ${branch#$tag_char$mark/} # remove the mark
 	elif [[ $add == false ]]; then continue
 	else
-		git branch -m $branch "#$mark/"${branch#$tag_char*/}
+		git branch -m $branch "$tag_char$mark/"${branch#$tag_char*/}
 	fi
 done
