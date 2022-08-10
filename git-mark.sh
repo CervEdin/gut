dac4a64b24807aca8685bc3f6adec9402b2acc1a
Erik Cervin-Edin <erik@cervined.in>
2022-08-10 12:01:12 +0200

    mark:	Modify script to pure bash, support deletion and multiple branches
    - Modify script to use bash variable substitution features.
    
    This may be reverted in the future. The project should strive for
    compatability.
    
    - Support deletion of "marks"
    
    It should be convinient to change/delete "marks". At the moment, a
    branch can have at most one mark and it starts with #.
    For example:
    `#a/x`
    means branch `x`, marked as `a`.
    
    - Multiple branches can be specified as a whitespace delimeted
      parameters after the mark
    
    For example:
    `git mark c '#a/x' '#b/y' 'z'`
    will mark branches `x`, `y`, `z` with mark `#c`.
    This will also overwrite marks `#a`/`#b` with `#c`.

--- git-mark.sh
+++ git-mark.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -euo
 
 if [ -z "$1" ]; then
@@ -6,8 +6,29 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
-mark="$1"
+add=true
+if [ "${1:0:1}" == "-" ]; then
+	mark="${1:1}"
+	add=false
+else
+	mark="$1"
+fi
+shift 1
 
-git branch --show-current |\
-	sed 's@^'"$mark"'/@@' |\
-	xargs -I% git branch -m "$mark"'/'%
+if [ -z "${1:-}" ]; then
+	branches="$(git branch --show-current)"
+else
+	branches="$@"
+fi
+
+for branch in $branches; do
+	# branch starts with #mark
+	if [[ $branch == "#$mark/"* ]]; then
+		[[ $add == true ]] &&
+			continue # already marked
+		git branch -m $branch ${branch#"#$mark/"} # remove the mark
+	elif [[ $add == false ]]; then continue
+	else
+		git branch -m $branch "#$mark/"${branch#'#'*/}
+	fi
+done
