9e08b09c71f83920605a1d743b089ff0bbf363fa
Erik Cervin-Edin <erik@cervined.in>
2022-09-06 10:42:11 +0200

    fix	Sorting parents not working for annotated tags
    Annoted tags do not have a committer date, instead they have a creator
    date.
    
    To resolve this, we add a sort by creator date after committer date.

--- git-parents.sh
+++ git-parents.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # sort by committerdate, latest first
 git for-each-ref --merged HEAD --no-contains HEAD \
 	--sort="-committerdate" \
+	--sort="-creatordate" \
 	--format="%(refname:short)" \
   -- 'refs/heads/' 'refs/tags/' |\
   grep -v 'archive/*'
