6f4097077a37a79a86ba22c906b213ed327eec7a
Erik Cervin-Edin <erik@cervined.in>
2022-10-04 10:20:02 +0200

    Separate listing of pruneable branches into separate script
    It's often desired to list the branches to be pruned. Extract this as a
    different script.

--- git-prune-local.sh
+++ git-prune-local.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-LC_ALL=C git for-each-ref --format='%(refname:short) %(upstream:track)' refs/heads/ |\
-	sed -n '/ \[gone\]$/{s@@@;p}' |\
-	xargs -I% git branch -D %
+git gone-local |
+	xargs git branch -D
