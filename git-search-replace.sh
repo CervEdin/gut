1fc09271dde333aacf42a9955ea8d891ffc97c2f
Erik Cervin-Edin <erik@cervined.in>
2022-10-23 20:18:44 +0200

    resolve: Fix	Default path to '.' using bash parameter expansion
    set -u will fail if "$3" is unassigned. Use paramter expansion instead

--- git-search-replace.sh
+++ git-search-replace.sh
@@ -3,11 +3,7 @@
 search_for="$1"
 replace_with="$2"
 
-if [ -z "${3}" ]; then
-	path='.'
-else
-	path="$3"
-fi
+path=${3:-.}
 
 git grep -l "$search_for" -- "$path" |\
 	xargs -d '\n' sed -i s@"$search_for"@"$replace_with"@g
