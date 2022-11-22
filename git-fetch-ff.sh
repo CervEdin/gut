103a71fe39556f36cc4e9ea139713afa9e635dac
Erik Cervin-Edin <erik@cervined.in>
2022-11-22 09:56:13 +0100

    syntax	Chop long arguments and multiline sed + awk scripts
    Long lines make diffs harder to follow.
    
    Break things up with more line-breaks.

--- git-fetch-ff.sh
+++ git-fetch-ff.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
 
-git for-each-ref --format='%(refname:short)%09%(upstream:track)%09%(upstream:short)' |
+git for-each-ref \
+	--format='%(refname:short)%09'\
+'%(upstream:track)%09'\
+'%(upstream:short)' |
 	column -t -s $'\t' |
-	sed -n '/ *\[behind [0-9]*\] */{s@@\t@;s@\(.*\)\t\(.*\)@\2\t\1:\1@;p}' |
-	awk -F '\t' 'NR==1{o[$1]=$2}NR>1{o[$1] = o[$1] FS $2}END{for (x in o) print x, o[x]}' |
+	sed -n '
+/ *\[behind [0-9]*\] */{
+	s@@\t@
+	s@\(.*\)\t\(.*\)@\2\t\1:\1@
+	p
+}' |
+	awk -F '\t' '
+NR==1 {
+	o[$1] = $2
+} NR>1 {
+	o[$1] = o[$1] FS $2
+} END {
+	for (x in o) print x, o[x]
+}' |
 	xargs git fetch
