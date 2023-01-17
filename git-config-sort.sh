7e9c3f33cf380da102f4a79c39003b0aa7bf47b8
Erik Cervin-Edin <erik@cervined.in>
2023-01-17 13:30:29 +0100

    config-sort:	make .gitconfig file into a parameter

--- git-config-sort.sh
+++ git-config-sort.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-awk '{ if ($1 ~ /^\[/) { section=$0; printf "%s\t\n", $0 } else { printf section; print $0 } }' .gitconfig |
+config="$1"
+
+awk '{ if ($1 ~ /^\[/) { section=$0; printf "%s\t\n", $0 } else { printf section; print $0 } }' "$config" |
 	sort -t ']' -k '1,1' |
 	sed '/\t$/{s@@@;p;d};s@^[^\t]*@@' |
-	sponge.sh .gitconfig
+	sponge.sh "$config"
