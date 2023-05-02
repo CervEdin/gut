6c360b165c6579e5a8819209569c913879f4e341
Erik Cervin-Edin <erik@cervined.in>
2023-05-02 22:23:21 +0200

    Sort, case insensitive

--- git-ignore.sh
+++ git-ignore.sh
@@ -5,4 +5,4 @@ usage=''
 printf '%s\n' "$@" |\
 	cat - .gitignore |\
 	sed '1{/^$/d};# skip empty first line if no args TODO: something better?' |\
-	sort -n -o .gitignore
+	sort -n -i -o .gitignore
