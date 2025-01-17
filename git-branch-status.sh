044de16bac42f09e2f5a05b5122c050f5d741ee4
Erik Cervin-Edin <erik@cervined.in>
2025-01-17 10:42:29 +0100

    gbs: Allow specifying args

--- git-branch-status.sh
+++ git-branch-status.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+# Default to refs/heads if no arguments are provided
+if [ "$#" -eq 0 ]; then
+    set -- "refs/heads"
+fi
+
 git for-each-ref \
-	--sort='committerdate' \
-	--format='%(refname:short)%09%(committerdate:short)%09%(upstream:track)%09%(upstream:remotename)' \
-	refs/heads |\
-	column -t -s '	'
+    --sort='committerdate' \
+    --format='%(refname:short)%09%(committerdate:short)%09%(upstream:track)%09%(upstream:remotename)' \
+    "$@" |\
+    column -t -s '	'
