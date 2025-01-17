32f207104bc783fe9dfe79c6ac370229693c3e21
Erik Cervin-Edin <erik@cervined.in>
2025-01-17 10:36:29 +0100

    White-space

--- git-branch-status.sh
+++ git-branch-status.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-git for-each-ref --format='%(refname:short)%09%(upstream:track)%09%(upstream:remotename)' refs/heads |\
+git for-each-ref \
+	--format='%(refname:short)%09%(upstream:track)%09%(upstream:remotename)' \
+	refs/heads |\
 	column -t -s '	'
