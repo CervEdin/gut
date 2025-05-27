4f671e579dcca4dcb3346b2dc0158e005d7ead57
Erik Cervin-Edin <erik@cervined.in>
2025-05-27 17:37:24 +0200

    White-space

--- git-branch-status.sh
+++ git-branch-status.sh
@@ -6,7 +6,7 @@ if [ "$#" -eq 0 ]; then
 fi
 
 git for-each-ref \
-	--sort='committerdate' \
-	--format='%(refname:short)%09%(committerdate:short)%09%(upstream:track)%09%(upstream:remotename)' \
-	"$@" |\
-	column -t -s '	'
+    --sort='committerdate' \
+    --format='%(refname:short)%09%(committerdate:short)%09%(upstream:track)%09%(upstream:remotename)' \
+    "$@" |\
+    column -t -s '	'
